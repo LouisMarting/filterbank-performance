31b2483942971f60b98d20e5546475c8a5139b96
Louis Marting <louie.marting@yahoo.com>
2022-06-08 10:04:41 +0200

    added analysis.py for tolerance analysis

---
+++ analysis.py
@@ -0,0 +1,8 @@
+import numpy as np
+import matplotlib as mpl
+from matplotlib import pyplot as plt
+
+from filterbank import DirectionalFilter,ReflectorFilter,ManifoldFilter,Filterbank
+
+def analyse(Filterbank : Filterbank,f):
+    pass
